bef186277421c75b3afbd2dda690b1b425ec1617
Mysta3 <myshawnestallings@gmail.com>
2022-07-21 19:35:11 -0700

    created checkout page along with checkout items and checkout cart functionality, removed unused files

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component.jsx';
-import './categories.styles.scss';
 import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
          <Route index element={ <Home/> } />
          <Route path='shop' element={ <Shop/> } />
          <Route path='auth' element={ <Authentication/> } />
+         <Route path='checkout' element={ <Checkout/> } />
       </Route>
     </Routes>
   );
