e8d589e567e6d4ae8176bf1c0bdf3a519e993bf8
Mysta3 <myshawnestallings@gmail.com>
2022-09-17 19:40:50 -0700

    migrated cart from Context to Redux

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,7 @@
-import { useContext } from 'react';
-import { CartContext } from '../contexts/cart.context';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCartItems } from '../../store/cart/cart.selector';
+import { addItemToCart, clearItemFromCart, removeItemFromCart } from '../../store/cart/cart.action';
+
 import {
   CheckoutItemContainer,
   ImageContainer,
@@ -13,11 +15,20 @@ import {
 
 const CheckoutItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
-  const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext);
+  const dispatch = useDispatch();
+  const cartItems = useSelector(selectCartItems);
+  
+  const clearItemHandler = () => dispatch(
+    clearItemFromCart(cartItems,cartItem)
+  );
 
-  const clearItemHandler = () => clearItemFromCart(cartItem);
-  const addItemHandler = () => addItemToCart(cartItem);
-  const removeItemHandler = () => removeItemFromCart(cartItem);
+  const addItemHandler = () => dispatch(
+    addItemToCart(cartItems,cartItem)
+  );
+
+  const removeItemHandler = () => dispatch(
+    removeItemFromCart(cartItems,cartItem)
+  );
 
   return (
     <CheckoutItemContainer>
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,8 @@
-import { Fragment, useContext } from 'react';
+import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
-import { CartContext } from '../../components/contexts/cart.context';
+import { selectIsCartOpen } from '../../store/cart/cart.selector';
 import { selectCurrentUser } from '../../store/user/user.selector';
 import { signOutUser } from '../../utils/firebase/firebase.utils.js';
 import CartIcon from '../../components/cart-icon/cart-icon.component';
@@ -11,7 +11,8 @@ import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component
 
 const Navigation = () => {
   const currentUser = useSelector(selectCurrentUser);
-  const { isCartOpen } = useContext(CartContext);
+  // const { isCartOpen } = useContext(CartContext);
+  const isCartOpen = useSelector(selectIsCartOpen);
   
   return(
     <Fragment>
