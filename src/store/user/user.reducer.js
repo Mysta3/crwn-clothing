4a95660bd29399c5555df85041b72c6282b3b564
Mysta3 <myshawnestallings@gmail.com>
2022-11-17 12:34:24 -0800

    implemented Check User Session Saga

--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,9 +1,7 @@
 import { USER_ACTION_TYPES } from './user.types';
 
 const INITIAL_STATE = {
-  currentUser: null,
-  isLoading: false,
-  error: null,
+  currentUser: null
 };
 
 export const userReducer = (state = INITIAL_STATE, action) => {
@@ -11,13 +9,11 @@ export const userReducer = (state = INITIAL_STATE, action) => {
 
   switch(type){
     case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
-      return { ...state, currentUser: payload };
-    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
-      return { ...state, currentUser: null };
-      // MULTI-matching
-    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+      return {
+        ...state,
+        currentUser: payload
+      }
     case USER_ACTION_TYPES.SIGN_IN_FAILED:
-    case USER_ACTION_TYPES.SIGN_UP_FAILED:
       return {...state, error: payload };
     default:
       return state;
