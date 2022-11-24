e045d17ab740b71288b2813bdb2d09692146c942
Myshawne Stallings <myshawnestallings@gmail.com>
2022-11-24 12:29:19 -0800

    Finishing CrwnClothing App
    * implemented Check User Session Saga
    
    * created sign in, sign up, sign out Saga functionality
    
    * initial stripe setup
    
    * paymentForm component setup
    
    * created serverless function
    
    * finish implementing paymentHandler function
    
    * setup spinner, cleaned up code

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import {CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from './checkout.styles.jsx';
 import { selectCartItems, selectCartTotal } from '../../store/cart/cart.selector';
+import PaymentForm from '../../components/payment-form/payment-form.component';
 
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
@@ -35,6 +36,7 @@ const Checkout = () => {
             )
           }
           <Total as='span'>Total: ${cartTotal}</Total>
+          <PaymentForm/>
       </CheckoutContainer>
       );
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -100,7 +100,7 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
       console.log('error creating user', error.message);
     }
   }
-  return userDocRef;
+  return userSnapshot;
 };
 
 
@@ -123,3 +123,16 @@ export const signInAuthEmailandPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged( //observer pattern
+      auth,
+      (userAuth) => {
+        unsubscribe();
+        resolve(userAuth);
+      },
+      reject
+    )
+  })
+};
