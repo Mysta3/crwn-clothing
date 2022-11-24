5077110378fb4e51987b421c25d8fab0a13f52e1
Mysta3 <myshawnestallings@gmail.com>
2022-11-24 11:51:04 -0800

    finish implementing paymentHandler function

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -16,12 +16,41 @@ const PaymentForm = () => {
 
     /// ADD SERVERLESS FUNCTIONS HERE
 
+    const response = await fetch('/.netlify/functions/create-payment-intent', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ amount: 100000 })
+    }).then(res => res.json());
+
+    const clientSecret = response.paymentIntent.client_secret
+    /*
+      OR you can destructure 
+      const { payment: { client_secret }} = response;
+    */
+    const paymentResult = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Cyber Chozen'
+        }
+      }
+    });
+
+    if(paymentResult.error){
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded'){
+        alert('Payment Successful');
+      }
+    }
   }
 
 
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}> Pay now </Button>
