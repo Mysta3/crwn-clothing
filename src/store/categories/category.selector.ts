299c49d34ee11ec9fcc7e0caafc87eacf32da5f5
Mysta3 <myshawnestallings@gmail.com>
2022-12-02 11:18:27 -0800

    typed out category selectors

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,7 +1,11 @@
 import { createSelector } from "reselect";
+import { CategoriesState } from "./category.reducer";
+import { CategoryMap } from "./category.types";
 
 
-const selectCategoryReducer = (state) => state.categories;
+
+
+const selectCategoryReducer = (state): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
     [selectCategoryReducer], // array of input selectors
@@ -9,16 +13,17 @@ export const selectCategories = createSelector(
     // the returned value of the input selector will be the argument for the 
     // output value. createSelector will only run when the categories object is the //same in memory
 );
+
 // AS LONG AS THE VALUE IS THE SAME, DO NOT RE-RUN THE createSelector
 // Memoization
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) => 
+    (categories): CategoryMap => 
         categories.reduce((acc, category) => {
             const { title, items } = category;
             acc[title.toLowerCase()] = items;
             return acc;
-        }, {})
+        }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
